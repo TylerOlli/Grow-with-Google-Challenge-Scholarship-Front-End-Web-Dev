27f6d522d36fcc5e6dd540143c498a237e779c49
TylerOlli <ollityler@gmail.com>
2018-04-06 19:32:07 -0400

    Finished inital template for Lite Brite

---
+++ Lesson-21/Lite-Brite/designs.js
@@ -0,0 +1,34 @@
+// Select color input
+
+function changeColor(){
+        $('td').click(function () {
+            $(this).css('background-color', $('#colorPicker').val());
+        });
+};
+
+// Select size input
+
+var table = document.getElementById("pixelCanvas");
+var numRows = document.querySelectorAll("tr").length;
+var numCols = document.querySelectorAll("td").length;
+
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid(){
+    $("#pixelCanvas tr").remove();
+    var inputHeight = document.getElementById("inputHeight").value;
+    var inputWeight = document.getElementById("inputWeight").value;
+    var numberHeight = Number(inputHeight);
+    var numberWeight = Number(inputWeight);
+
+    for (var i = numRows; i < numberHeight; i++) {
+        var ii = 0;
+        $('#pixelCanvas').append("<tr>");
+        
+    };
+
+    for (var i = numCols; i < numberWeight; i++) {
+        $('tr').append("<td class='add' onclick='changeColor()'>");
+    };
+
+};
